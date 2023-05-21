784079f1a7e1d54b079b4eef3af2adf252a2b568
Pavel Glukhov <pvglukhov@gmail.com>
2023-05-22 02:16:27 +0300

    [master] initial commit

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+public class Dollar {
+    public int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiple) {
+        this.amount *= multiple;
+    }
+}
