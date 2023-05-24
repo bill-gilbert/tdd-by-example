a332165bd270599bae10d272dbbec5c9c6ef9a7d
Pavel Glukhov <pvglukhov@gmail.com>
2023-05-24 19:59:32 +0300

    [master] added rate

--- src/main/java/guru/springframework/Bank.java
+++ src/main/java/guru/springframework/Bank.java
@@ -1,7 +1,24 @@
 package guru.springframework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+    private Map<Pair, Integer> rateMap = new HashMap<>();
+
     Money reduce(Expression source, String toCurrency) {
-        return source.reduce(toCurrency);
+        return source.reduce(this, toCurrency);
+    }
+
+    void addRate(String from, String to, int rate) {
+        this.rateMap.put(new Pair(from,to), rate);
+    }
+
+    public int rate(String from, String to) {
+        if (from.equals(to)) {
+            return 1;
+        }
+
+        return this.rateMap.get(new Pair(from,to));
     }
 }
