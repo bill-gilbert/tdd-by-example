dea505bc34f31ee68f3736f848466d21677979b7
Pavel Glukhov <pgluhov@verci.ru>
2025-11-12 17:48:24 +0300

    added chapeter 3 code

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -10,4 +10,17 @@ public class Dollar {
     public Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hashCode(amount);
+//    }
 }
--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -1,7 +1,9 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class MoneyTest {
 
@@ -9,9 +11,14 @@ class MoneyTest {
     void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        Assertions.assertEquals(10, product.amount);
+        assertEquals(10, product.amount);
         product = five.times(3);
-        Assertions.assertEquals(15, product.amount);
+        assertEquals(15, product.amount);
+    }
 
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
     }
 }
